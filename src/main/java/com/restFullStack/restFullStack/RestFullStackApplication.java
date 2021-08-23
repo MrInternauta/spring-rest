8d777816767ae73501e7561a45e9266593733132
Felipe Ramirez <feldjesus@gmail.com>
2021-08-23 16:38:04 -0500

    +Was added the project files

---
+++ src/main/java/com/restFullStack/restFullStack/RestFullStackApplication.java
@@ -0,0 +1,12 @@
+package com.restFullStack.restFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullStackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/restFullStack/restFullStack/RestFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restFullStack.restFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
